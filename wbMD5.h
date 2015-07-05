57414001ba65e32d592cf99efb0e6893cb8372e5
Abdul Dakkak <abduld@wolfram.com>
2015-07-05 02:08:56 +0200

    add helper function wbMD5

--- wbMD5.h
+++ wbMD5.h
@@ -37,25 +37,23 @@
 
 #ifndef __WB_MD5_H__
 #define __WB_MD5_H__
- 
+
 #include <string.h>
- 
 
 /* Any 32-bit or wider unsigned integer data type will do */
 typedef unsigned int MD5_u32plus;
- 
+
 typedef struct {
   MD5_u32plus lo, hi;
   MD5_u32plus a, b, c, d;
   unsigned char buffer[64];
   MD5_u32plus block[16];
 } MD5_CTX;
- 
-static void MD5_Init(MD5_CTX *ctx);
-static void MD5_Update(MD5_CTX *ctx, const void *data, unsigned long size);
-static void MD5_Final(unsigned char *result, MD5_CTX *ctx);
 
- 
+static void wbMD5_Init(MD5_CTX *ctx);
+static void wbMD5_Update(MD5_CTX *ctx, const void *data, unsigned long size);
+static void wbMD5_Final(unsigned char *result, MD5_CTX *ctx);
+
 /*
  * The basic MD5 functions.
  *
@@ -63,20 +61,20 @@ static void MD5_Final(unsigned char *result, MD5_CTX *ctx);
  * architectures that lack an AND-NOT instruction, just like in Colin Plumb's
  * implementation.
  */
-#define F(x, y, z)      ((z) ^ ((x) & ((y) ^ (z))))
-#define G(x, y, z)      ((y) ^ ((z) & ((x) ^ (y))))
-#define H(x, y, z)      (((x) ^ (y)) ^ (z))
-#define H2(x, y, z)     ((x) ^ ((y) ^ (z)))
-#define I(x, y, z)      ((y) ^ ((x) | ~(z)))
- 
+#define F(x, y, z) ((z) ^ ((x) & ((y) ^ (z))))
+#define G(x, y, z) ((y) ^ ((z) & ((x) ^ (y))))
+#define H(x, y, z) (((x) ^ (y)) ^ (z))
+#define H2(x, y, z) ((x) ^ ((y) ^ (z)))
+#define I(x, y, z) ((y) ^ ((x) | ~(z)))
+
 /*
  * The MD5 transformation for all four rounds.
  */
-#define STEP(f, a, b, c, d, x, t, s) \
-  (a) += f((b), (c), (d)) + (x) + (t); \
-  (a) = (((a) << (s)) | (((a) & 0xffffffff) >> (32 - (s)))); \
+#define STEP(f, a, b, c, d, x, t, s)                                           \
+  (a) += f((b), (c), (d)) + (x) + (t);                                         \
+  (a) = (((a) << (s)) | (((a)&0xffffffff) >> (32 - (s))));                     \
   (a) += (b);
- 
+
 /*
  * SET reads 4 input bytes in little-endian byte order and stores them
  * in a properly aligned word in host byte order.
@@ -86,45 +84,40 @@ static void MD5_Final(unsigned char *result, MD5_CTX *ctx);
  * doesn't work.
  */
 #if defined(__i386__) || defined(__x86_64__) || defined(__vax__)
-#define SET(n) \
-  (*(MD5_u32plus *)&ptr[(n) * 4])
-#define GET(n) \
-  SET(n)
+#define SET(n) (*(MD5_u32plus *)&ptr[(n)*4])
+#define GET(n) SET(n)
 #else
-#define SET(n) \
-  (ctx->block[(n)] = \
-  (MD5_u32plus)ptr[(n) * 4] | \
-  ((MD5_u32plus)ptr[(n) * 4 + 1] << 8) | \
-  ((MD5_u32plus)ptr[(n) * 4 + 2] << 16) | \
-  ((MD5_u32plus)ptr[(n) * 4 + 3] << 24))
-#define GET(n) \
-  (ctx->block[(n)])
+#define SET(n)                                                                 \
+  (ctx->block[(n)] = (MD5_u32plus)ptr[(n)*4] |                                 \
+                     ((MD5_u32plus)ptr[(n)*4 + 1] << 8) |                      \
+                     ((MD5_u32plus)ptr[(n)*4 + 2] << 16) |                     \
+                     ((MD5_u32plus)ptr[(n)*4 + 3] << 24))
+#define GET(n) (ctx->block[(n)])
 #endif
- 
+
 /*
  * This processes one or more 64-byte data blocks, but does NOT update
  * the bit counters.  There are no alignment requirements.
  */
-static const void *body(MD5_CTX *ctx, const void *data, unsigned long size)
-{
+static const void *body(MD5_CTX *ctx, const void *data, unsigned long size) {
   const unsigned char *ptr;
   MD5_u32plus a, b, c, d;
   MD5_u32plus saved_a, saved_b, saved_c, saved_d;
- 
+
   ptr = (const unsigned char *)data;
- 
+
   a = ctx->a;
   b = ctx->b;
   c = ctx->c;
   d = ctx->d;
- 
+
   do {
     saved_a = a;
     saved_b = b;
     saved_c = c;
     saved_d = d;
- 
-/* Round 1 */
+
+    /* Round 1 */
     STEP(F, a, b, c, d, SET(0), 0xd76aa478, 7)
     STEP(F, d, a, b, c, SET(1), 0xe8c7b756, 12)
     STEP(F, c, d, a, b, SET(2), 0x242070db, 17)
@@ -141,8 +134,8 @@ static const void *body(MD5_CTX *ctx, const void *data, unsigned long size)
     STEP(F, d, a, b, c, SET(13), 0xfd987193, 12)
     STEP(F, c, d, a, b, SET(14), 0xa679438e, 17)
     STEP(F, b, c, d, a, SET(15), 0x49b40821, 22)
- 
-/* Round 2 */
+
+    /* Round 2 */
     STEP(G, a, b, c, d, GET(1), 0xf61e2562, 5)
     STEP(G, d, a, b, c, GET(6), 0xc040b340, 9)
     STEP(G, c, d, a, b, GET(11), 0x265e5a51, 14)
@@ -159,8 +152,8 @@ static const void *body(MD5_CTX *ctx, const void *data, unsigned long size)
     STEP(G, d, a, b, c, GET(2), 0xfcefa3f8, 9)
     STEP(G, c, d, a, b, GET(7), 0x676f02d9, 14)
     STEP(G, b, c, d, a, GET(12), 0x8d2a4c8a, 20)
- 
-/* Round 3 */
+
+    /* Round 3 */
     STEP(H, a, b, c, d, GET(5), 0xfffa3942, 4)
     STEP(H2, d, a, b, c, GET(8), 0x8771f681, 11)
     STEP(H, c, d, a, b, GET(11), 0x6d9d6122, 16)
@@ -177,8 +170,8 @@ static const void *body(MD5_CTX *ctx, const void *data, unsigned long size)
     STEP(H2, d, a, b, c, GET(12), 0xe6db99e5, 11)
     STEP(H, c, d, a, b, GET(15), 0x1fa27cf8, 16)
     STEP(H2, b, c, d, a, GET(2), 0xc4ac5665, 23)
- 
-/* Round 4 */
+
+    /* Round 4 */
     STEP(I, a, b, c, d, GET(0), 0xf4292244, 6)
     STEP(I, d, a, b, c, GET(7), 0x432aff97, 10)
     STEP(I, c, d, a, b, GET(14), 0xab9423a7, 15)
@@ -195,89 +188,84 @@ static const void *body(MD5_CTX *ctx, const void *data, unsigned long size)
     STEP(I, d, a, b, c, GET(11), 0xbd3af235, 10)
     STEP(I, c, d, a, b, GET(2), 0x2ad7d2bb, 15)
     STEP(I, b, c, d, a, GET(9), 0xeb86d391, 21)
- 
+
     a += saved_a;
     b += saved_b;
     c += saved_c;
     d += saved_d;
- 
+
     ptr += 64;
   } while (size -= 64);
- 
+
   ctx->a = a;
   ctx->b = b;
   ctx->c = c;
   ctx->d = d;
- 
+
   return ptr;
 }
- 
 
-static void MD5_Init(MD5_CTX *ctx)
-{
+static void wbMD5_Init(MD5_CTX *ctx) {
   ctx->a = 0x67452301;
   ctx->b = 0xefcdab89;
   ctx->c = 0x98badcfe;
   ctx->d = 0x10325476;
- 
+
   ctx->lo = 0;
   ctx->hi = 0;
 }
- 
 
-static void MD5_Update(MD5_CTX *ctx, const void *data, unsigned long size)
-{
+static void wbMD5_Update(MD5_CTX *ctx, const void *data, unsigned long size) {
   MD5_u32plus saved_lo;
   unsigned long used, available;
- 
+
   saved_lo = ctx->lo;
   if ((ctx->lo = (saved_lo + size) & 0x1fffffff) < saved_lo)
     ctx->hi++;
   ctx->hi += size >> 29;
- 
+
   used = saved_lo & 0x3f;
- 
+
   if (used) {
     available = 64 - used;
- 
+
     if (size < available) {
       memcpy(&ctx->buffer[used], data, size);
       return;
     }
- 
+
     memcpy(&ctx->buffer[used], data, available);
     data = (const unsigned char *)data + available;
     size -= available;
     body(ctx, ctx->buffer, 64);
   }
- 
+
   if (size >= 64) {
     data = body(ctx, data, size & ~(unsigned long)0x3f);
     size &= 0x3f;
   }
- 
+
   memcpy(ctx->buffer, data, size);
 }
- 
-static void MD5_Final(unsigned char *result, MD5_CTX *ctx)
-{
+
+static void wbMD5_Final(unsigned char *result, MD5_CTX *ctx) {
   unsigned long used, available;
- 
+
   used = ctx->lo & 0x3f;
- 
+
   ctx->buffer[used++] = 0x80;
- 
+
   available = 64 - used;
- 
+
   if (available < 8) {
     memset(&ctx->buffer[used], 0, available);
     body(ctx, ctx->buffer, 64);
     used = 0;
     available = 64;
   }
- 
+
   memset(&ctx->buffer[used], 0, available - 8);
- 
+
   ctx->lo <<= 3;
   ctx->buffer[56] = ctx->lo;
   ctx->buffer[57] = ctx->lo >> 8;
@@ -287,9 +275,9 @@ static void MD5_Final(unsigned char *result, MD5_CTX *ctx)
   ctx->buffer[61] = ctx->hi >> 8;
   ctx->buffer[62] = ctx->hi >> 16;
   ctx->buffer[63] = ctx->hi >> 24;
- 
+
   body(ctx, ctx->buffer, 64);
- 
+
   result[0] = ctx->a;
   result[1] = ctx->a >> 8;
   result[2] = ctx->a >> 16;
@@ -306,8 +294,29 @@ static void MD5_Final(unsigned char *result, MD5_CTX *ctx)
   result[13] = ctx->d >> 8;
   result[14] = ctx->d >> 16;
   result[15] = ctx->d >> 24;
- 
+
   memset(ctx, 0, sizeof(*ctx));
 }
- 
+
+static void wbMD5(const char *str, size_t length, unsigned char *digest) {
+  int n;
+  MD5_CTX c;
+
+  MD5_Init(&c);
+
+  while (length > 0) {
+    if (length > 512) {
+      MD5_Update(&c, str, 512);
+    } else {
+      MD5_Update(&c, str, length);
+    }
+    length -= 512;
+    str += 512;
+  }
+
+  MD5_Final(digest, &c);
+
+  return ;
+}
+
 #endif /* __WB_MD5_H__ */
