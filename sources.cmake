501d8029e7d0d527399745673fd74fb3cd3c8312
Abdul Dakkak <abduld@wolfram.com>
2016-03-17 14:14:40 -0500

    simplify cmake file

--- sources.cmake
+++ sources.cmake
@@ -1,55 +1,29 @@
 
-list(APPEND LIBWB_HEADER_FILES
-    ${CMAKE_CURRENT_LIST_DIR}/wb.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbArg.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbAssert.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbCUDA.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbCast.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbComparator.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbDataGenerator.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbDirectory.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbExit.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbExport.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbFile.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbImage.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbImport.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbInit.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbLogger.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbMD5.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbMPI.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbMalloc.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbPPM.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbSolution.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbSparse.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbString.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbThrust.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbTimer.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbTypes.h
-    ${CMAKE_CURRENT_LIST_DIR}/wbUtils.h
+file(GLOB THESE_CPP_FILES
+    ${CMAKE_CURRENT_LIST_DIR}/*.cpp
 )
+
+file(GLOB THESE_TEST_FILES
+    ${CMAKE_CURRENT_LIST_DIR}/*_test.cpp
+)
+
+list(REMOVE_ITEM THESE_CPP_FILES ${THESE_TEST_FILES})
+
+file(GLOB THESE_HEADER_FILES
+    ${CMAKE_CURRENT_LIST_DIR}/*.h
+)
+
+list(APPEND LIBWB_HEADER_FILES
+    ${THESE_HEADER_FILES}
+)
+
 list(APPEND LIBWB_SOURCE_FILES
-    ${CMAKE_CURRENT_LIST_DIR}/wbArg.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbCUDA.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbDataGenerator.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbDirectory.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbExit.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbExport.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbFile.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbImage.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbImport.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbInit.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbLogger.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbMPI.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbPPM.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbSolution.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbSparse.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbTimer.cpp
+    ${THESE_CPP_FILES}
     ${CMAKE_CURRENT_LIST_DIR}/vendor/json11.cpp
 )
 
 list(APPEND LIBWB_TEST_FILES
-    ${CMAKE_CURRENT_LIST_DIR}/wb_test.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/wbDataGenerator_test.cpp
+    ${THESE_TEST_FILES}
 )
 
 if (BUILD_LIBWB_LIBRARY)
