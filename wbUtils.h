74caed84f8d658f61a8ba09f4a25e10654fefb7c
Abdul Dakkak <dakkak@illinois.edu>
2016-03-28 22:02:48 -0500

    on unix the session id is picked up from the SESSION_ID environment variable

--- wbUtils.h
+++ wbUtils.h
@@ -19,16 +19,13 @@ static std::string _sessionId{};
 static std::string sessionId() {
 #ifdef WB_USE_UNIX
   if (_sessionId != "") {
-    const char *env = std::getenv("SESSION_ID");
+    char *env = std::getenv("SESSION_ID");
     if (env) {
       _sessionId = env;
     }
   }
-}
-return _sessionId;
-#else  /* WB_USE_UNIX */
-  return _sessionId;
 #endif /* WB_USE_UNIX */
+  return _sessionId;
 }
 
 #endif /* __WB_UTILS_H__ */
