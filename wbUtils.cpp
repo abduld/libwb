708a2c5e4f60710adc44d4ffc4dd71f5daa7af48
Abdul Dakkak <dakkak@illinois.edu>
2016-03-31 08:57:19 -0500

    move uuid to header file

--- wbUtils.cpp
+++ wbUtils.cpp
@@ -1,8 +1,2 @@
 
 #include "wb.h"
-#include "vendor/sole.hpp"
-
-std::string uuid() {
-  auto u4 = sole::uuid4();
-  return u4.str();
-}
--- wbUtils.h
+++ wbUtils.h
@@ -1,12 +1,17 @@
 #ifndef __WB_UTILS_H__
 #define __WB_UTILS_H__
 
+#include "vendor/sole.hpp"
+
 #ifdef WB_DEBUG
 #define DEBUG(...) __VA_ARGS__
 #else /* WB_DEBUG */
 #define DEBUG(...)
 #endif /* WB_DEBUG */
 
-std::string uuid();
+static std::string uuid() {
+  auto u4 = sole::uuid4();
+  return u4.str();
+}
 
 #endif /* __WB_UTILS_H__ */
